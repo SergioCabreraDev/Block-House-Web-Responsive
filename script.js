99218ec643b020eb27cba8090a359fa4d8046567
SergioCabreraDev <scabrera.main@gmail.com>
2023-08-26 10:58:31 +0200

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var overlay = document.getElementById("overlay");
+    var entendidoButton = document.getElementById("entendido");
+
+    entendidoButton.addEventListener("click", function() {
+        overlay.style.display = "none";
+    });
+});
